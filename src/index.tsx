ccc89f194d6aa0b034ff26b8b4dc9b827a1a2d5c
Kristo Veber <kristoveber7@gmail.com>
2024-02-13 13:35:16 +0200

    Add Apollo Client for GraphQL integration

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppWrapper from './App';
+import {ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
+import Cookies from 'universal-cookie';
+
+const httpLink = createHttpLink({
+  uri: 'https://01.kood.tech/api/graphql-engine/v1/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = new Cookies().get('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AppWrapper />
+    <ApolloProvider client={client}>
+      <AppWrapper />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
