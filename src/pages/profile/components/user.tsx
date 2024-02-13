4b94e52d091fa58f4f97918db5ebb22581e0e7de
Kristo Veber <kristoveber7@gmail.com>
2024-02-13 13:36:34 +0200

    Add User component to profile page

---
+++ src/pages/profile/components/user.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+
+const GET_USER = gql`
+query {
+    user {
+        id
+        login
+      }
+ }
+`
+
+export const User = () => {
+    const {error, data, loading} = useQuery(GET_USER)
+
+    console.log({error, data, loading})
+    if (loading) return <div>Loading...</div>
+    return (
+       <div>
+            <h1 className='text-3xl'>Welcome, {data.user[0].login}!</h1>
+       </div>
+    )
+}
